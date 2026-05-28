d5da18a1e9d01614841f6a45962b44155069d20b
datnq2001 <datnq2001@gmail.com>
2026-05-28 14:08:28 +1000

    fix(deploy): lazy load image routes

--- nutrihelp_ai/routers/__init__.py
+++ nutrihelp_ai/routers/__init__.py
@@ -1,7 +1,5 @@
-# from .image_api import router as image_api
 from .medical_report_api import router as medical_report_api
 from .chatbot_api import router as chatbot_api
-from .image_api import router as image_api
 from .health_plan_api import router as health_plan_api
 from .finetune_api import router as finetune_api
 from .meal_plan_api import router as meal_plan_api
@@ -10,7 +8,6 @@ from .meal_log_api import router as meal_log_api
 __all__ = [
     "medical_report_api",
     "chatbot_api",
-    "image_api",
     "health_plan_api",
     "finetune_api",
     "meal_plan_api",
